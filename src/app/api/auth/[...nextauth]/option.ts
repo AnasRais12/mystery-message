6dcaac10b7108c846a2c1c09e98135355975951f
Anas Baig <anasraees33@gmail.com>
2024-10-15 22:41:45 +0500

    Auth-Start

--- src/app/api/auth/[...nextauth]/option.ts
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -1,19 +1,19 @@
-import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials"
-import bcrypt from 'bcryptjs'
-import dbConnect from "@/lib/dbConnect";
-import UserModel from "@/model/user";
+// import { NextAuthOptions } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials"
+// import bcrypt from 'bcryptjs'
+// import dbConnect from "@/lib/dbConnect";
+// import UserModel from "@/model/user";
  
-export const authOptions:NextAuthOptions ={
-    providers:[
-        CredentialsProvider({
-            id: "credentials", 
-            name: "Credentials",
-            credentials:{
-                
-            }
-        })
-    ]
-}
+// export const authOptions:NextAuthOptions ={
+//     providers:[
+//         CredentialsProvider({
+//             id: "credentials", 
+//             name: "Credentials",
+//             credentials:{
+
+//             }
+//         })
+//     ]
+// }
 
 
